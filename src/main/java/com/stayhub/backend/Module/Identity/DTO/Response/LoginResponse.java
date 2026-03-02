c6a672bd24a624e5bc5ee790a324422d5f5c5084
tuanteoo <nvt167workhard@gmail.com>
2026-03-02 14:59:03 +0700

    feat(auth): return user information in response

--- src/main/java/com/stayhub/backend/Module/Identity/DTO/Response/LoginResponse.java
+++ src/main/java/com/stayhub/backend/Module/Identity/DTO/Response/LoginResponse.java
@@ -6,5 +6,6 @@ public record LoginResponse(
         String accessToken,
         String refreshToken,
         List<String> roles,
-        String status
+        String status,
+        UserInfResponse userInfResponse
 ) { }

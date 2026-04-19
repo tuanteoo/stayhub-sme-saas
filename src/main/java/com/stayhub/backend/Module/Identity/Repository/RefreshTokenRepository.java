e585c22b9e31ad90af9eb75fbbfb0e3d031ec292
tuanteoo <nvt167workhard@gmail.com>
2026-04-19 09:16:10 +0700

    chore: add  @Transactional for deleteOldTokens

--- src/main/java/com/stayhub/backend/Module/Identity/Repository/RefreshTokenRepository.java
+++ src/main/java/com/stayhub/backend/Module/Identity/Repository/RefreshTokenRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -18,6 +19,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     @Query("DELETE FROM RefreshToken r WHERE r.token = :token")
     void deleteByToken(String token);
 
+    @Transactional
     @Modifying
     @Query("DELETE FROM RefreshToken r WHERE r.user = :user AND r.deviceInfo = :deviceInfo")
     void deleteOldTokens(User user, String deviceInfo);

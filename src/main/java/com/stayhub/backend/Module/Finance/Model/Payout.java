2f1cf5db852c9d33d279462c38df02c474309d6e
tuanteoo <nvt167workhard@gmail.com>
2026-04-19 09:45:37 +0700

    fix(finance): remove column update_at in payout Model

--- src/main/java/com/stayhub/backend/Module/Finance/Model/Payout.java
+++ src/main/java/com/stayhub/backend/Module/Finance/Model/Payout.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Payout extends AbstractEntity {
+public class Payout {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

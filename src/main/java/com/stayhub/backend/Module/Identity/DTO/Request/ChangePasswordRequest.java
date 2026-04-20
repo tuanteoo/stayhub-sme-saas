dd2a8f51849e91a30c814fc6554a2ca4dde92846
tuanteoo <nvt167workhard@gmail.com>
2026-04-20 14:09:52 +0700

    feat(auth): add api change password

---
+++ src/main/java/com/stayhub/backend/Module/Identity/DTO/Request/ChangePasswordRequest.java
@@ -0,0 +1,29 @@
+package com.stayhub.backend.Module.Identity.DTO.Request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ChangePasswordRequest(
+        @NotBlank(message = "Mật khẩu cũ không được để trống")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$",
+                message = "Mật khẩu phải dài ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt"
+        )
+        String oldPassword,
+
+        @NotBlank(message = "Mật khẩu mới không được để trống")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$",
+                message = "Mật khẩu phải dài ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt"
+        )
+        String newPassword,
+
+        @NotBlank(message = "Xác nhận mật khẩu không được để trống")
+        @Pattern(
+                regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$",
+                message = "Mật khẩu phải dài ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt"
+        )
+        String confirmPassword
+) {
+}
--- src/main/java/com/stayhub/backend/Module/Identity/Repository/RefreshTokenRepository.java
+++ src/main/java/com/stayhub/backend/Module/Identity/Repository/RefreshTokenRepository.java
@@ -26,4 +26,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     @Modifying
     @Query("DELETE FROM RefreshToken r WHERE r.expiryDate < CURRENT_TIMESTAMP")
     int deleteExpiredTokens();
+
+    void deleteByUser(User user);
 }

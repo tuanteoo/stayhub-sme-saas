052e837c55c3d57e421e961b5260381f8a692739
tuanteoo <nvt167workhard@gmail.com>
2026-04-21 20:16:29 +0700

    refactor(booking); use Specification git list dispute of booking

--- src/main/java/com/stayhub/backend/Module/Booking/Repository/DisputeRepository.java
+++ src/main/java/com/stayhub/backend/Module/Booking/Repository/DisputeRepository.java
@@ -1,25 +1,28 @@
 package com.stayhub.backend.Module.Booking.Repository;
 
-import com.stayhub.backend.Common.Util.DisputeStatus;
 import com.stayhub.backend.Module.Booking.Model.Dispute;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface DisputeRepository extends JpaRepository<Dispute,Long> {
+public interface DisputeRepository extends JpaRepository<Dispute,Long>, JpaSpecificationExecutor<Dispute> {
     boolean existsByBookingId(Long bookingId);
 
-    @EntityGraph(attributePaths = {"booking", "creator", "creator.roles"})
-    @Query("SELECT d FROM Dispute d")
-    Page<Dispute> findAllWithGraph(Pageable pageable);
-
-    @EntityGraph(attributePaths = {"booking", "creator", "creator.roles"})
-    @Query("SELECT d FROM Dispute d WHERE d.status = :status")
-    Page<Dispute> findByStatusWithGraph(DisputeStatus status, Pageable pageable);
+    @EntityGraph(attributePaths = {
+            "booking",
+            "booking.user",
+            "booking.property.host",
+            "creator",
+            "creator.roles"
+    })
+    @Override
+    @NonNull
+    Page<Dispute> findAll(@Nullable Specification<Dispute> spec, @NonNull Pageable pageable);
 }

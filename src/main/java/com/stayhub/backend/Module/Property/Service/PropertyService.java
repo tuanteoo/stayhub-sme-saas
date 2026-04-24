c32c2d4a8d23716ab40c4bee5f1b3da95ab05f50
tuanteoo <nvt167workhard@gmail.com>
2026-04-24 16:46:08 +0700

    chore: revert commit, cancel ingration sepay

--- src/main/java/com/stayhub/backend/Module/Property/Service/PropertyService.java
+++ src/main/java/com/stayhub/backend/Module/Property/Service/PropertyService.java
@@ -20,4 +20,5 @@ public interface PropertyService {
     PropertyDetailResponse convertToDetailResponse(Property property, LocalDate checkInDate, LocalDate checkOutDate);
     PageResponse<AdminPropertyResponse> getPropertiesForAdmin(String status, int page, int size, String sortBy, String sortDir);
     HostPropertyStatsResponse getHostPropertyStats(Long hostId);
+    void togglePropertyStatus(Long hostId, Long propertyId, boolean isActive);
 }
